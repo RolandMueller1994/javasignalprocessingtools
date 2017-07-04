cbb1766cb2e8116c7af4949406fb997468162ccb
RolandMueller1994 <roland.mueller.1994@freenet.de>
2017-07-04 10:01:05 +0200

    Added testcase

---
+++ javasignalprocessingtools/src/test/java/filtertest/FIRFilterTest.java
@@ -0,0 +1,14 @@
+package filtertest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FIRFilterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
