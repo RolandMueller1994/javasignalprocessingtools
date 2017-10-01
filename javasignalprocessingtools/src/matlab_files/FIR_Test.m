f26b53379cd5c9ef0a49de3703a44956b92fef75
AndreEngelmann <an_engelmann@t-online.de>
2017-10-01 15:46:37 +0200

    test for fir filter

--- javasignalprocessingtools/src/matlab_files/FIR_Test.m
+++ javasignalprocessingtools/src/matlab_files/FIR_Test.m
@@ -10,4 +10,16 @@ hlp = sinc(2*Fg*tau); %calculation of coefs
 y_canon = f_filter_canon2( hlp, y); 
 
 % import file 
+filename = 'C:\Users\Andre\Documents\GitHub\javasignalprocessingtools\javasignalprocessingtools\src\matlab_files\FIRFilterTest.txt';
+delimiter = ' ';
+formatSpec = '%f%f%[^\n\r]';
+fileID = fopen(filename,'r');
+dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'MultipleDelimsAsOne', true, 'TextType', 'string',  'ReturnOnError', false);
+fclose(fileID);
+FIRFilterTest = table(dataArray{1:end-1}, 'VariableNames', {'x_java','y_java'});
+x_java = FIRFilterTest.x_java';
+y_java = FIRFilterTest.y_java'; 
+clearvars filename delimiter formatSpec fileID dataArray ans;
 
+plot(x, y)
+plot(x, y_canon, x_java, y_java)
