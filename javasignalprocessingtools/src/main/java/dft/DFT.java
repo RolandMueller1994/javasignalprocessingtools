4c82a341bd5c5ae319103c755bfcf0909a8cd14d
RolandMueller1994 <roland.mueller.1994@freenet.de>
2017-11-23 20:33:46 +0100

    Javadoc problems

--- javasignalprocessingtools/src/main/java/dft/DFT.java
+++ javasignalprocessingtools/src/main/java/dft/DFT.java
@@ -3,38 +3,45 @@ package dft;
 import javax.annotation.Nonnull;
 
 /**
- * This Class implements a method for calculating the discrete fourier transformation (DFT) of input values.
+ * This Class implements a method for calculating the discrete fourier
+ * transformation (DFT) of input values.
  * 
  * @author Andre
  *
  */
 public class DFT {
 	/**
-	 * This Class implements a method for calculating the discrete fourier transformation (DFT) of input values.
+	 * This Class implements a method for calculating the discrete fourier
+	 * transformation (DFT) of input values.
 	 */
 	public DFT() {
 
 	}
+
 	/**
-	 * Calculates the DFT of the input array. 
+	 * Calculates the DFT of the input array.
 	 * 
-	 * @param Array of {@linkplain int}
+	 * @param input
+	 * 
 	 * @return Array of {@linkplain ComplexNumber}
 	 */
 	public @Nonnull ComplexNumber[] calcDFT(int[] input) {
 		return calcDFT(ComplexNumber.intToComplex(input), false);
 	}
+
 	/**
-	 * Calculates the DFT of the input array. 
+	 * Calculates the DFT of the input array.
 	 * 
-	 * @param Array of input values of type {@linkplain ComplexNumber}
-	 * @param inverse. Select DFT (False) or Invers-DFT (True)
+	 * @param input
+	 *            of input values of type {@linkplain ComplexNumber}
+	 * @param inverse
+	 *            Select DFT (False) or Invers-DFT (True)
 	 * @return Array of {@linkplain ComplexNumber}
 	 */
 
 	public @Nonnull ComplexNumber[] calcDFT(ComplexNumber[] input, boolean inverse) {
 		ComplexNumber[] output = new ComplexNumber[input.length];
-		try{
+		try {
 			int n = input.length;
 			double q = 2 * Math.PI / n;
 			double c, s, si, at, bt, t, w;
@@ -54,18 +61,17 @@ public class DFT {
 					bt = c * bt - s * at + input[n - i - 2].getImag();
 					at = t;
 				}
-				output[j]= new ComplexNumber(c * at + s * bt + input[0].getReal(), c * bt - s * at + input[0].getImag());
+				output[j] = new ComplexNumber(c * at + s * bt + input[0].getReal(),
+						c * bt - s * at + input[0].getImag());
 			}
 			return output;
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 			output = new ComplexNumber[1];
-			output[0] = new ComplexNumber(0,0); 
+			output[0] = new ComplexNumber(0, 0);
 			return output;
-			
-		}	
-		finally {
+
+		} finally {
 		}
 	}
 }
